2e29c743a7b3c80fe588cdf5d336608db27bddf9
MikhaskoS <mikhassergej@yandex.ru>
2020-05-27 09:51:41 +0300

    Рефакторинг. Удаление ветвей.

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Utility;
+using Utility.Sorting;
+
+
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] ar = new double[] { 5, 4, 9, 0, 10 };
+
+            BubbleSort.Sort(ar);
+
+            ar.PrintArray();
+        }
+    }
+}
--- Utility/Sorting/BubbleSort.cs
+++ Utility/Sorting/BubbleSort.cs
@@ -1,43 +1,67 @@
-﻿namespace Utility.Sorting
+﻿using System;
+
+namespace Utility.Sorting
 {
     public class BubbleSort
     {
         /// <summary>
         /// Пузырьковая сортировка
         /// </summary>
-        public static void Sort(int[] A)
-        {
-            for (int i = 0; i < A.Length; i++)
-            {
-                for (int j = A.Length - 1; j > i; j--)
-                {
-                    if (A[j] < A[j - 1])
-                    {
-                        //меняем местами (без дополнительной переменной)
-                        A[j] = A[j - 1] + A[j];
-                        A[j - 1] = A[j] - A[j - 1];
-                        A[j] = A[j] - A[j - 1];
-                    }
-                }
-            }
-        }
+        //public static void Sort(int[] A)
+        //{
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        for (int j = A.Length - 1; j > i; j--)
+        //        {
+        //            if (A[j] < A[j - 1])
+        //            {
+        //                //меняем местами (без дополнительной переменной)
+        //                A[j] = A[j - 1] + A[j];
+        //                A[j - 1] = A[j] - A[j - 1];
+        //                A[j] = A[j] - A[j - 1];
+        //            }
+        //        }
+        //    }
+        //}
 
         /// <summary>
         /// Пузырьковая сортировка (оптимизированная)
         /// </summary>
-        public static void SortOptimize(int[] A)
+        //public static void SortOptimize(int[] A)
+        //{
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        int permutation = 0;
+        //        for (int j = A.Length - 1; j > i; j--)
+        //        {
+        //            if (A[j] < A[j - 1])
+        //            {
+        //                //меняем местами (без дополнительной переменной)
+        //                A[j] = A[j - 1] + A[j];
+        //                A[j - 1] = A[j] - A[j - 1];
+        //                A[j] = A[j] - A[j - 1];
+
+        //                permutation++;
+        //            }
+        //        }
+        //        // если перестановки прекратились - на выход
+        //        if (permutation <= 1) break;
+        //    }
+        //}
+
+        public static void Sort<T>(T[] A) where T : IComparable<T>
         {
             for (int i = 0; i < A.Length; i++)
             {
                 int permutation = 0;
                 for (int j = A.Length - 1; j > i; j--)
                 {
-                    if (A[j] < A[j - 1])
+                    if (A[j].CompareTo(A[j-1]) < 0)  // в порядке возрастания
                     {
                         //меняем местами (без дополнительной переменной)
-                        A[j] = A[j - 1] + A[j];
-                        A[j - 1] = A[j] - A[j - 1];
-                        A[j] = A[j] - A[j - 1];
+                        T temp = A[j];
+                        A[j] = A[j - 1];
+                        A[j - 1] = temp;
 
                         permutation++;
                     }
